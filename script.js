1ca9fc494f7926764da1dfb918a325e1d7cd4b0a
Ritesh Kishor Chaudhari <100477678+Ritu0097@users.noreply.github.com>
2024-02-04 18:13:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ var dateDiffInDays = function (date1, date2) {
 		return 0;
 	}
 	else{
-		return Math.floor(d2-d1);
+		return Math.floor((d2-d1)/1000*60*60*24);
 	}
 };
 
-Do not change the code below.
-const dateOne = prompt("Enter Start Date.");
-const dateTwo = prompt("Enter End Date.");
-alert(dateDiffInDays(dateOne, dateTwo));
+// Do not change the code below.
+// const dateOne = prompt("Enter Start Date.");
+// const dateTwo = prompt("Enter End Date.");
+// alert(dateDiffInDays(dateOne, dateTwo));
